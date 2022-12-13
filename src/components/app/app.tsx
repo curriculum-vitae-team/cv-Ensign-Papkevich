bdffa61cbe5f5cf7bca150f88edf679967a17a43
alensign <alexandra.wilchitskaya@gmail.com>
2022-12-13 14:03:54 +0100

    update theme

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,11 +1,16 @@
 import { ApolloProvider } from "@apollo/client"
 import { client } from "../../graphql/client"
 import { Router } from "../router/router"
+import { theme } from "./app.theme"
+import { ThemeProvider, CssBaseline } from "@mui/material"
 
 export const App = () => {
   return (
     <ApolloProvider client={client}>
-      <Router />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Router />
+      </ThemeProvider>
     </ApolloProvider>
   )
 }
