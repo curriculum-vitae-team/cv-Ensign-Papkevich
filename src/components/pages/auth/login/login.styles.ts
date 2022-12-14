768f98e8a8a411d8117e0810c7d92edc188fb9a0
alensign <alexandra.wilchitskaya@gmail.com>
2022-12-14 15:27:33 +0100

    fix @material-ui/core/ not compatible with react18

--- src/components/pages/auth/login/login.styles.ts
+++ src/components/pages/auth/login/login.styles.ts
@@ -1,5 +1,5 @@
 import { styled } from "@mui/material"
-import { Button, TextField, Link } from "@material-ui/core/"
+import { Button, TextField, Link } from "@mui/material"
 
 export const form = styled("form")({
   width: "100%",
