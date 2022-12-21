d137d605d739a6d9746e36fd3e3eaa05ae66aa64
George P <gonnaa1@gmail.com>
2022-12-21 01:05:39 +0100

    Auth service impl and other minor changes

--- src/components/pages/auth/login/login.tsx
+++ src/components/pages/auth/login/login.tsx
@@ -1,30 +1,40 @@
 import { useLazyQuery } from "@apollo/client"
-import { Typography } from "@mui/material"
+import { IconButton, InputAdornment, Typography } from "@mui/material"
 import { LOGIN_QUERY } from "../../../../graphql/queries"
 import { useNavigate } from "react-router-dom"
 import * as Styled from "./login.styles"
 import { useForm } from "react-hook-form"
-import { access_token } from "../../auth/reactiveComponent"
-
-//TODO make show pass btn
-//TODO validate that email or pass field is not empty
+import { securityService } from "../../../../security/securityService"
+import { AuthFormValues } from "../auth.types"
+import { useState } from "react"
+import RemoveRedEyeIcon from "@mui/icons-material/RemoveRedEye"
+import VisibilityOffIcon from "@mui/icons-material/VisibilityOff"
 
 export const Login = () => {
   const navigate = useNavigate()
 
-  //TODO use interface type after merge
-  const { register, handleSubmit } = useForm()
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<AuthFormValues>({
+    defaultValues: { email: "", password: "" },
+  })
+
+  const [showPassword, setShowPassword] = useState(false)
+  const handleClickShowPassword = () => setShowPassword(!showPassword)
+  const handleMouseDownPassword = () => setShowPassword(!showPassword)
 
   const [doLogIn] = useLazyQuery(LOGIN_QUERY, {
     onCompleted: ({ login }) => {
-      access_token(login.access_token)
+      securityService.writeToStorage(login.access_token)
       navigate("/example")
     },
   })
   return (
     <Styled.Form
-      //TODO is this okay???
       onSubmit={handleSubmit((data) => {
+        console.log("wow")
         doLogIn({
           variables: {
             email: data.email,
@@ -32,20 +42,51 @@ export const Login = () => {
           },
         })
       })}
-      //TODO is this okay???
     >
       <Typography variant="h2">Welcome back!</Typography>
       <Styled.TextFieldMod
-        {...register("email")}
+        error={!!errors.email}
+        helperText={errors?.email?.message}
+        {...register("email", {
+          required: "This field is required",
+          pattern: {
+            value:
+              /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+            message: "Please enter a valid email",
+          },
+        })}
         id="outlined-basic"
         label="login"
         variant="outlined"
       />
       <Styled.PasswordField
-        {...register("password")}
+        error={!!errors.password}
+        helperText={errors?.password?.message}
+        {...register("password", {
+          required: "This field is required",
+          validate: (value: string) => {
+            if (value.length < 5) {
+              return "Password must be be at least 5 characters"
+            }
+          },
+        })}
         id="outlined-basic"
         label="password"
         variant="outlined"
+        type={showPassword ? "text" : "password"}
+        InputProps={{
+          endAdornment: (
+            <InputAdornment position="end">
+              <IconButton
+                aria-label="toggle password visibility"
+                onClick={handleClickShowPassword}
+                onMouseDown={handleMouseDownPassword}
+              >
+                {showPassword ? <VisibilityOffIcon /> : <RemoveRedEyeIcon />}
+              </IconButton>
+            </InputAdornment>
+          ),
+        }}
       />
       <Styled.LinkMod href="/auth/signup" underline="none">
         Doesn't have an account yet? Register now!
